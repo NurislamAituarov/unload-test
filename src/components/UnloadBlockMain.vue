cb5e83a10fecff14a5b89945af1444ab3566c442
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-10-24 15:37:34 +0600

    refactor style / add adaptive

---
+++ src/components/UnloadBlockMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block block-main">
+    <h2 class="text-bold">Выгрузка</h2>
+    <p class="text-bold subtitle">Выполняет работу:</p>
+    <span> - Собирает фотографии из заказов пользователей. </span>
+    <span> - Выгружает по папкам</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.block-main {
+  height: min-content;
+  .subtitle {
+    font-size: 16px;
+  }
+  span {
+    display: block;
+  }
+}
+</style>
